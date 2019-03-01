29209dcc35cb20b1e60fe0fc26bb4f485e6b6e79
emeth- <seanybob@gmail.com>
2019-02-28 17:01:18 -0800

    port tweaks

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const PORT = process.env.PORT || 5000;
-//const PORT = 80;
 var dnsd = require('dnsd')
 dnsd.createServer(function(req, res) {
   console.log("HIT");
   res.end('1.2.3.4')
-}).listen(PORT, '127.0.0.1')
-console.log('Server running at 127.0.0.1:'+PORT)
+}).listen(process.env.PORT || 5000)
+console.log('Server running at 127.0.0.1:'+(process.env.PORT || 5000))
 //node index.js
 //https://github.com/iriscouch/dnsd
 //npm install dnsd
