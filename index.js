d47039e7927a3e43a8bd42a1c64b9fca37b3c857
emeth- <seanybob@gmail.com>
2019-02-28 16:44:12 -0800

    add hit message

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const PORT = process.env.PORT || 5000;
 var dnsd = require('dnsd')
 dnsd.createServer(function(req, res) {
+  console.log("HIT");
   res.end('1.2.3.4')
 }).listen(PORT, '127.0.0.1')
 console.log('Server running at 127.0.0.1:'+PORT)
